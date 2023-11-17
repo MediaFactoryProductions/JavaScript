7f7a09aeee6ce4e7e9ea17c8770710d4df28a5e8
MediaFactoryProductions <mediafactoryprod@gmail.com>
2023-11-17 10:43:38 -0600

    Update ternary_operator.js

--- ternary_operator.js
+++ ternary_operator.js
@@ -1,3 +1,7 @@
-let value = 'This a string';
+let number = 10;
+let result = (number % 2 == 0) ? "Even" : "Odd";
+console.log(result);
+
+let value = 42;
 let type = (typeof value === 'number') ? 'Number' : 'Not a Number';
 console.log(type); // Output will be "Number" since value is a number
